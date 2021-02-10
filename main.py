2bc31134db380c55e39bd2f0730abae0393e6dde
joates <77863872+joates-spring-2021@users.noreply.github.com>
2021-02-10 13:19:58 -0800

    Updated project
    Added text files
    Added code template

---
+++ main.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+
+johnwoates
+CPSC 223P-01
+Wed Feb 10, 2021
+joates@fullerton.edu
+
+"""
+
+# Maybe you can use some functions from the string module
+import string
+
+# This is the function that you must write the code for
+def numWordsSpoken(candidate, word):
+    "This is a doc string that describes the function. Change it to your liking."
+
+    # When a function is empty, this line is required to avoid a syntax error
+    # When you add code here, remove "pass"
+    pass
+
+
+# This code will extract the data from the input files
+debateFile = open("debate.txt", "r")
+debateString = debateFile.read() 
+
+stopWordsFile = open("stopWords.txt", "r")
+stopWordsString = stopWordsFile.read()
+
+
+# Start your code here
+
+    
