d3ff24b49c1a7269dd0cd9e8a239f9d9d4fd2f08
joates <77863872+joates-spring-2021@users.noreply.github.com>
2021-02-10 18:54:48 -0800

    Minor changes

--- main.py
+++ main.py
@@ -13,21 +13,27 @@ import string
 
 # This is the function that you must write the code for
 def numWordsSpoken(candidate, word):
-    "This is a doc string that describes the function. Change it to your liking."
+    """"This is a doc string that describes the function. Change it to your liking.
+    Something like: This function returns the number of times a given word was 
+    spoken by a given speaker"""
 
     # When a function is empty, this line is required to avoid a syntax error
     # When you add code here, remove "pass"
     pass
 
 
-# This code will extract the data from the input files
+# This code will extract the data from the debate file and read it into one big
+# string named debateString
 debateFile = open("debate.txt", "r")
 debateString = debateFile.read() 
+debateFile.close()
 
+# This code will extract the data from the stop words file and read it into one big
+# string named StopWordsString
 stopWordsFile = open("stopWords.txt", "r")
 stopWordsString = stopWordsFile.read()
+stopWordsFile.close()
 
 
 # Start your code here
 
-    
